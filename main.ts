d2539342c82a1740796d1235a32614e94dea9650
Eliott Wantz <70651737+EliottWantz@users.noreply.github.com>
2024-10-11 22:16:55 -0400

    add generation script

--- main.ts
+++ main.ts
@@ -1,11 +1,23 @@
+import { ensureDir } from "@std/fs";
 import path, { dirname } from "node:path";
 import { fileURLToPath } from "node:url";
 
+type CodeInput = {
+  code: number;
+  phrase: string;
+  constant: string;
+  comment: {
+    doc: string;
+    description: string;
+  };
+};
+
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const CODES_PATH = path.join(__dirname, "codes.jsonc");
-const TS_FILE_PATH = path.join(__dirname, "index.ts");
+const TS_CODES_PATH = path.join(__dirname, "dist", "codes.ts");
+const TS_PHRASES_PATH = path.join(__dirname, "dist", "phrases.ts");
 
-async function downloadJsonCodes() {
+async function downloadJsonCodes(): Promise<void> {
   const response = await fetch(
     "https://raw.githubusercontent.com/prettymuchbryce/http-status-codes/refs/heads/master/codes.json"
   );
@@ -19,7 +31,7 @@ async function downloadJsonCodes() {
   await Deno.writeTextFile(CODES_PATH, codesContent);
 }
 
-async function readCodesJsonFile() {
+async function readCodesJsonFile(): Promise<CodeInput[]> {
   const codesData = await Deno.readTextFile(CODES_PATH);
   const start = codesData.indexOf("\n") + 1;
   const codes = JSON.parse(codesData.slice(start));
@@ -29,41 +41,33 @@ async function readCodesJsonFile() {
 async function generateStatusCodes() {
   const codes = await downloadJsonCodes().then(readCodesJsonFile);
 
-  let tsContent = `// Generated from from https://raw.githubusercontent.com/prettymuchbryce/http-status-codes/refs/heads/master/codes.json at ${new Date().toISOString()}. Do not edit!\n\n`;
+  await ensureDir("./dist");
+
+  let codesTsContent = `// Generated from https://raw.githubusercontent.com/prettymuchbryce/http-status-codes/refs/heads/master/codes.json at ${new Date().toISOString()}. Do not edit!\n\n`;
+  let phrasesTsContent = `// Generated from https://raw.githubusercontent.com/prettymuchbryce/http-status-codes/refs/heads/master/codes.json at ${new Date().toISOString()}. Do not edit!\n\n`;
 
   // Generate individual exports
   for (const code of codes) {
-    tsContent += `/**\n`;
-    tsContent += ` * ${code.comment.doc}\n`;
-    tsContent += ` *\n`;
-    tsContent += ` * ${code.comment.description}\n`;
-    tsContent += ` *\n`;
-    tsContent += ` */\n`;
-    tsContent += `export const ${code.constant} = {\n`;
-    tsContent += `  code: ${code.code} as const,\n`;
-    tsContent += `  phrase: "${code.phrase}" as const,\n`;
-    tsContent += `  constant: "${code.constant}" as const,\n`;
-    tsContent += `} as const;\n\n`;
+    const jsDoc = `/**
+ * ${code.comment.doc}
+ *
+ * ${code.comment.description}
+ *
+ */
+`;
+    codesTsContent += jsDoc;
+    phrasesTsContent += jsDoc;
+    codesTsContent += `export const ${code.constant} = ${code.code} as const;\n\n`;
+    phrasesTsContent += `export const ${code.constant} = "${code.phrase}" as const;\n\n`;
   }
 
-  // Generate HTTP_STATUS_CODES object
-  tsContent += "// HTTP Status Codes\n\n";
-  tsContent += "export const HTTP_STATUS_CODES = {\n";
-  for (const code of codes) {
-    tsContent += `  ${code.constant},\n`;
-  }
-  tsContent += "} as const;\n\n";
+  await Deno.writeTextFile(TS_CODES_PATH, codesTsContent);
+  await Deno.writeTextFile(TS_PHRASES_PATH, phrasesTsContent);
 
-  // Add type declarations
-  tsContent += `export type StatusCode = typeof HTTP_STATUS_CODES;\n`;
-  tsContent += `export type StatusCodeKey = keyof StatusCode;\n`;
-
-  await Deno.writeTextFile(TS_FILE_PATH, tsContent);
-
-  console.log("TypeScript file 'http-status-codes.ts' has been generated.");
+  console.log(`TypeScript file ${TS_PHRASES_PATH} has been generated.`);
 }
 
 // Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
 if (import.meta.main) {
-  downloadJsonCodes().catch(console.error);
+  generateStatusCodes().catch(console.error);
 }
